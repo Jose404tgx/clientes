ba561c5e065e5fd049cd0aa8d3cf47ceb66bedc2
Jose404tgx <thegrax@gmail.com>
2026-04-15 12:17:17 -0500

    eliminar clientes

--- eliminar.php
+++ eliminar.php
@@ -2,30 +2,19 @@
 require 'conexion.php';
 
 if ($_POST) {
-    $tipo = $_POST['tipo'];
     $id = $_POST['id'];
 
-
-    if ($tipo == "proveedor") {
-        $sql = "DELETE FROM proveedor WHERE id_proveedor=:id";
-    }
-
+    $sql = "DELETE FROM clientes WHERE id=:id";
     $stmt = $conexion->prepare($sql);
     $stmt->execute([':id' => $id]);
 
-    echo "Registro eliminado";
+    echo "Cliente eliminado correctamente";
 }
 ?>
 
-<h2>Eliminar</h2>
+<h2>Eliminar Cliente</h2>
 
 <form method="POST">
     ID: <input type="number" name="id"><br>
-
-    <select name="tipo">
-      
-        <option value="proveedor">Proveedor</option>
-    </select><br><br>
-
     <button type="submit">Eliminar</button>
 </form>
